c081bb464f2dc6dfdb4015e8616d0414a74af923
aqsakhannn <159409677+aqsakhannn@users.noreply.github.com>
2024-02-24 11:09:53 -0700

    20.listing

---
+++ mountain_list.js
@@ -0,0 +1,4 @@
+"use strict";
+let mountain = ['K2', 'Kohekoram', 'Nanga parbat', 'Himaliyas'];
+console.log('list of mountain:');
+console.log(mountain);
---
+++ mountain_list.ts
@@ -0,0 +1,3 @@
+let mountain : string[] = ['K2','Kohekoram','Nanga parbat','Himaliyas'];
+console.log('list of mountain:');
+console.log(mountain);
