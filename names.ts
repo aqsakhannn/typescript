a2ae9684bd77f5869e7b75f9545799473dd957ac
aqsakhannn <159409677+aqsakhannn@users.noreply.github.com>
2024-02-24 13:39:31 -0700

    12.names

---
+++ names.ts
@@ -0,0 +1,16 @@
+
+
+
+
+let names : string[] = ['Imran Khan','Nawaz sharif', 'kamran khan tessori','Shehbaz sharif'];
+ 
+const message : string = 'Welcome to typescript class: '
+
+
+for( let i=0; i<names.length; i++){
+    
+
+
+    console.log(message + names[i]);
+
+}
