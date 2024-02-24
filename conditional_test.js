f847f6bd5eb234c9cc58ba7e80bef759f7066640
aqsakhannn <159409677+aqsakhannn@users.noreply.github.com>
2024-02-24 11:04:35 -0700

    23.conditional_test

---
+++ conditional_test.js
@@ -0,0 +1,26 @@
+"use strict";
+// Print a statement
+//describing each test and your prediction for the results of each test. Your code
+//should look something like this:
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+// Look closely at your results, and make sure you understand why each line evaluates to True or False.
+//Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+console.log(car == 'sozo');
+let model = 20;
+console.log("Is model == 20 ? I predict True.");
+console.log(model == 20);
+console.log(model == 19);
+let color = 'Black';
+console.log("Is color == Black ? I predict True.");
+console.log(color == 'blue');
+console.log(color == 'Black');
+let engine = 'auto';
+console.log("Is engine == auto? I predict True.");
+console.log(engine == 'basic');
+console.log(engine == 'auto');
+let speed = 120;
+console.log("Is speed == 120 ? I predict True.");
+console.log(speed == 20);
+console.log(speed == 120);
