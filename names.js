b9fa94e11766340d21e5d32fb14bf54390a2898c
aqsakhannn <159409677+aqsakhannn@users.noreply.github.com>
2024-02-24 13:40:15 -0700

    11.Names

--- names.js
+++ names.js
@@ -1,6 +1,5 @@
 "use strict";
 let names = ['Imran Khan', 'Nawaz sharif', 'kamran khan tessori', 'Shehbaz sharif'];
-const message = 'Welcome to typescript class: ';
 for (let i = 0; i < names.length; i++) {
-    console.log(message + names[i]);
+    console.log(names[i]);
 }
--- names.ts
+++ names.ts
@@ -3,14 +3,11 @@
 
 
 let names : string[] = ['Imran Khan','Nawaz sharif', 'kamran khan tessori','Shehbaz sharif'];
- 
-const message : string = 'Welcome to typescript class: '
-
 
 for( let i=0; i<names.length; i++){
     
 
 
-    console.log(message + names[i]);
+    console.log(names[i]);
 
 }
