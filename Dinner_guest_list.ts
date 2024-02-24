3698b005abe07db1b1048ed1d089f935e7611275
aqsakhannn <159409677+aqsakhannn@users.noreply.github.com>
2024-02-24 11:10:53 -0700

    19.Dinner_guest-list

---
+++ Dinner_guest_list.ts
@@ -0,0 +1,29 @@
+
+let Guest_list : string[] = ['imran Khan','Nawaz shahrif','Shehbaz Sharif','Asif Ali ZArdari'];
+
+  for(let i=0; i<Guest_list.length; i++){
+
+
+    //console.log('Dear Mr, ' + Guest_list[i] + ',\n\nIt is our pleasure to invite you in our party.\n\n\
+    //Thanks You\n\n');
+
+
+  //let absent_Guest : string = 'imran Khan' ;
+
+
+  //let new_Guest : string = 'Kamran Khan Tessori' ;
+
+  //Guest_list[0] = new_Guest ;
+
+  // for(let i=0; i<Guest_list.length; i++){
+
+ //   console.log('Dear Mr, ' + Guest_list[i] + ',\n\nIt is our pleasure to invite you in our party.\n\n\Thanks You\n\n');
+ //}
+ //console.log(`${absent_Guest} is not coming into the party.`);
+
+
+
+   //print a message indicating the number of people you are inviting to dinner
+
+    console.log ('Dear Mr, ' +  Guest_list[i] + '\n\nYou are inviting to dinner.\n\n\Thank you');
+  }
