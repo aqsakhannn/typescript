f890acb45ece3434262a61610a8b7e870ce7c5a7
aqsakhannn <159409677+aqsakhannn@users.noreply.github.com>
2024-02-24 13:37:35 -0700

    29.favFruit

---
+++ fav_fruit.ts
@@ -0,0 +1,37 @@
+
+
+// Make a array of your three favorite fruits and call it favorite_fruits.
+let favFruit : string[] = ['Mango','Grapes','Pomegrant'] 
+//Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement,
+//such as You really like bananas!
+
+if(favFruit[0] == 'banana'){
+    console.log('i like bananas')
+} else{
+    console.log('I dont like Bananas.')
+
+}
+if(favFruit[0] === 'Mango'){
+    console.log('I like Mango')
+} else{
+    console.log('I dont like Mango.')
+    
+}
+if(favFruit[1] === 'cherry'){
+    console.log('I like Cherry')
+} else{
+    console.log('I dont like Cherry.')
+    
+}
+if(favFruit[1] === 'Grapes'){
+    console.log('I like Grapes')
+} else{
+    console.log('I dont like Grapes.')
+    
+}
+if(favFruit[2] === 'Guava'){
+    console.log('I like Guava')
+} else{
+    console.log('I dont like Guava.')
+    
+}
