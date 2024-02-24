7f49fd9326c0306b0d4daac2bdf72dc4c4b2ec3c
aqsakhannn <159409677+aqsakhannn@users.noreply.github.com>
2024-02-24 11:59:01 -0700

    15.guest-list

---
+++ guest_list.js
@@ -0,0 +1,5 @@
+"use strict";
+let Guest_list = ['Nawaz shahrif', 'Shehbaz Sharif', 'Asif Ali ZArdari'];
+for (let i = 0; i < Guest_list.length; i++) {
+    console.log('Dear Mr, ' + Guest_list[i] + ',\n\nIt is our pleasure to invite you in our party.\n\n\Thanks You');
+}
---
+++ guest_list.ts
@@ -0,0 +1,9 @@
+
+
+let Guest_list : string[] = ['Nawaz shahrif','Shehbaz Sharif','Asif Ali ZArdari'];
+
+for(let i=0; i<Guest_list.length; i++){
+
+
+    console.log('Dear Mr, ' + Guest_list[i] + ',\n\nIt is our pleasure to invite you in our party.\n\n\Thanks You');
+}
