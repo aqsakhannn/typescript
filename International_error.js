26786f85d67434218a821cefadc3442b5f7642e3
aqsakhannn <159409677+aqsakhannn@users.noreply.github.com>
2024-02-24 11:08:41 -0700

    22.international_erorr

---
+++ International_error.js
@@ -0,0 +1,7 @@
+"use strict";
+//Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs
+//to produce an index error.
+let Days = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'friday'];
+//console.log(Days[7]);
+// Mak let personalData : {name : string, fname : string, age : number, isvote : boolean } = {name : 'Aisha', fname : 'female', age : 'twelve', isvote : 'no' };
+console.log(Days[6]);
---
+++ International_error.ts
@@ -0,0 +1,6 @@
+  //Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs
+  //to produce an index error.
+  let Days : string[] = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'friday'];
+  //console.log(Days{7});
+  // Mak let personalData : {name : string, fname : string, age : number, isvote : boolean } = {name : 'Aisha', fname : 'female', age : 'twelve', isvote : 'no' };
+   console.log(Days[6]);
