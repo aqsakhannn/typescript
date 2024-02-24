2098c60f4e45b8bbaf0ac6998b5eae27417b56e1
aqsakhannn <159409677+aqsakhannn@users.noreply.github.com>
2024-02-24 13:38:56 -0700

    13.transportation

---
+++ transportation.js
@@ -0,0 +1,5 @@
+"use strict";
+let transportation = ['motorcycle', 'car', 'BMW', 'train'];
+for (let i = 0; i < transportation.length; i++) {
+    console.log('i like to own a ' + transportation[i]);
+}
---
+++ transportation.ts
@@ -0,0 +1,12 @@
+
+
+
+let transportation : string[] = ['motorcycle','car','BMW','train'] ;
+
+for(let i=0; i<transportation.length; i++){ 
+
+    console.log('i like to own a ' + transportation[i])
+
+
+
+}
